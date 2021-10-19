f8ade27599b7f4b9f0890072b802bee39f61555a
Mongkol_Narkkong <toonnerryner@gmail.com>
2021-10-19 17:38:28 +0700

    next step

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -2,8 +2,8 @@ import "./intro.scss"
 
 const Intro = () => {
     return (
-        <div className="intro">
-            {/* Intro */}
+        <div className="intro" id="intro">
+            Intro
         </div>
     )
 }
--- src/components/Topbar/Topbar.jsx
+++ src/components/Topbar/Topbar.jsx
@@ -3,6 +3,14 @@ import "./topbar.scss"
 const Topbar = () => {
     return (
         <div className="topbar">
+            <div className="wrapper"> {/* all topbar component*/}
+                <div className="left">{/*logo and discription*/}
+                    <a href="#intro">LOGO HERE</a> {/*link to intro (start point in web) maping by id*/}
+                </div>
+                <div className="right">{/* hamburger menu*/}
+
+                </div>
+            </div>
         </div>
     )
 }
